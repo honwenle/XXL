f21d61b1e1cd7faa166557ca500b07cb8ce37590
洪文乐 <noclip@foxmail.com>
2017-08-09 17:34:11 +0800

    补间 官方参考

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var game = new Phaser.Game('95', '95', Phaser.AUTO, '', {
     preload: preload,
-    create: create,
-    update: update
+    create: create
 });
 var ROWS, COLS;
 var GEM_SIZE = 70;
 var gems;
+var selectedGem = null;
+var startXY = {x: 0, y: 0};
 function preload () {
     game.load.spritesheet("GEMS", "timg.png", GEM_SIZE, GEM_SIZE);
 }
@@ -18,7 +19,14 @@ function create () {
         for (var j = 0; j < ROWS; j++) {
             var gem = gems.create(i * GEM_SIZE, j * GEM_SIZE, 'GEMS');
             gem.frame = game.rnd.integerInRange(0, gem.animations.frameTotal - 1);
+            gem.inputEnabled = true;
+            gem.events.onInputDown.add(selectGem, this);
+            gem.events.onInputUp.add(releaseGem, this);
         }
     }
 }
-function update () {}
+function selectGem (gem) {
+    selectedGem = gem;
+    game.add.tween(selectedGem).to({x:0},200,'Linear',true)
+}
+function releaseGem () {}
